9852869d8c9ad4596cf21c2c668d62f65d848a4b
diamondrarktvo <diamondra.rktvo@gmail.com>
2022-10-21 17:58:18 +0300

    essai débug

--- app/utils/services/urlAxios.js
+++ app/utils/services/urlAxios.js
@@ -1,6 +1,11 @@
 import Axios from 'axios';
+import { Platform } from 'react-native';
 
-const url = 'http://10.0.2.2:3000/api/v1';
+// const url = 'http://10.0.2.2:3000/api/v1';
+const url =
+   Platform.OS === 'android'
+      ? 'http://10.0.2.2:3000/api/v1'
+      : 'http://localhost:3000/api/v1';
 // const uRI = "http://localhost:3000/api/v1"
 
 const RouteAxios = Axios.create({
